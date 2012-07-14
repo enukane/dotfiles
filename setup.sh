48b4437d17d2e532d49370d70303e11362c7edc8
enukane <enukane@glenda9.org>
2012-07-14 17:10:26 +0900

    fix wrong mv in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo move current .zshrc to .zshrc.bak
+echo move current dotfiles/.zshrc to .zshrc.bak
 [ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
 echo make symlink to zshrc
-ln -s zshrc ~/.zshrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
 
 echo move current .vimrc to .vimrc.bak
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
 echo make symlink to vimrc
-ln -s vimrc ~/.vimrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
 
 echo move current .vim to .vim.bak
-[ -f ~/.vim ] && mv ~/.vim ~/.vim.bak
+[ -d ~/.vim ] && mv ~/.vim ~/.vim.bak
 echo make symlink to vimrc
-ln -s vim ~/.vim
+ln -s ~/dotfiles/vim ~/.vim
