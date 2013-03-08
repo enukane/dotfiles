d78d34a85037be3908bacd1fbe05b4bf8ebb1258
enukane <enukane@glenda9.org>
2013-03-08 15:56:57 +0900

    setup.sh backups more gently: backup file won't remove older backup

--- setup.sh
+++ setup.sh
@@ -1,30 +1,32 @@
 #!/bin/sh
 
-echo move current dotfiles/.zshrc to .zshrc.bak
-[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak
+DATE=`date "+%Y%m%d%H%M.%S"`
+
+echo move current dotfiles/.zshrc to .zshrc.bak.$DATE
+[ -f ~/.zshrc ] && mv ~/.zshrc ~/.zshrc.bak.$DATE
 echo make symlink to zshrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 
-echo move current .vimrc to .vimrc.bak
-[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak
+echo move current .vimrc to .vimrc.bak.$DATE
+[ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bak.$DATE
 echo make symlink to vimrc
 ln -s ~/dotfiles/vimrc ~/.vimrc
 
-echo move current .vim to .vim.bak
-[ -d ~/.vim ] && mv ~/.vim ~/.vim.bak
+echo move current .vim to .vim.bak.$DATE
+[ -d ~/.vim ] && mv ~/.vim ~/.vim.bak.$DATE
 echo make symlink to vimrc
 ln -s ~/dotfiles/vim ~/.vim
 
 echo make swap dir for vim
 mkdir ~/.swp
 
-echo move current .screenrc to .screenrc.bak
-[ -f ~/.screenrc ] && mv ~/.screenrc ~/.screenrc.bak
+echo move current .screenrc to .screenrc.bak.$DATE
+[ -f ~/.screenrc ] && mv ~/.screenrc ~/.screenrc.bak.$DATE
 echo make symlink to screenrc
 ln -s ~/dotfiles/screenrc ~/.screenrc
 
-echo move current .tmux.conf to .tmux.conf
-[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
+echo move current .tmux.conf to .tmux.conf.bak.$DATE
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak.$DATE
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 
 echo "##########################################################"
