ae7877c14fa299ac6a48741a4cc7bacb4203542e
enukane <enukane@glenda9.org>
2014-03-20 16:44:16 +0900

    fix fbsdsetup

--- fbsd-setup.sh
+++ fbsd-setup.sh
@@ -2,10 +2,9 @@
 
 # run as root
 # install git & sudo first
-
-pkg install zsh
-pkg install vim
-pkg install tmux
+sudo pkg install zsh
+sudo pkg install vim
+sudo pkg install tmux
 
 git config --global user.email enukane@glenda9.org
 git config --global user.name enukane
