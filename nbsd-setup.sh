88cc6dcacb6a17db6f6066e3246049ef5ef75013
enukane <enukane@glenda9.org>
2014-03-21 23:50:21 +0900

    nsbd-setup: remove xmonad, add git certs
    Signed-off-by: enukane <enukane@glenda9.org>

--- nbsd-setup.sh
+++ nbsd-setup.sh
@@ -7,12 +7,18 @@ sudo pkgin install vim
 sudo pkgin install tmux
 sudo pkgin install wget
 sudo pkgin install the_silver_searcher
-sudo pkgin install xmonad
+# no xmonad!
 
 git config --global user.email enukane@glenda9.org
 git config --global user.name enukane
 
 chsh -s `which zsh`
+
+mkdir ~/certs
+curl http://curl.haxx.se/ca/cacert.pem -o ~/certs/cacert.pem
+echo "[http]" >> ~/.gitconfig
+echo "sslCAinfo= ${HOME}/certs/cacert.pem" >> ~/.gitconfig
+
 
 echo "also don't forget to run..."
 echo "setup.sh and visudo"
